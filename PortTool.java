bf3b57c4e6406da4f07dbda401b44d89bf6e9178
Benjamin Struhl <struhl.b@husky.neu.edu>
2017-06-29 12:49:48 -0400

    Intial Template
    No real code but something to start off with

---
+++ PortTool.java
@@ -0,0 +1,29 @@
+public class PortTool {
+  
+  
+  
+  private static void main(String[] args) {
+
+    // holds amount of files in the staging area
+    int filesWaiting = 0;
+    // holds the path to where the data is being stored
+    String pathToData = "";
+    
+    // check if their is anything to import
+    if(filesWaitng > 0 ) {
+      exportFiles(pathToData);
+    } else {
+      System.out.println("Found no files to");
+    }
+    
+  }
+  
+  // void -> void 
+  // goes through the specificed directory and prints out any
+  // documents to the PaperPort tool; 
+  private static void exportFiles(String path) {
+    System.out.println("test");
+  }
+  
+  
+}
